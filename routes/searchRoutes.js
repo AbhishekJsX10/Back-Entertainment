ecd43a903372bd76b5ea30fc3644cf02d2740b64
Abhishek10usedit <avishekchaturvedi0@gmail.com>
2024-05-04 22:03:02 +0530

    commits

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,31 +1,31 @@
 
-// import express from'express'
-// import Movies from "../models/movieModel.js"
+// import express from 'express';
+// import Movies from "../models/movieModel.js";
 // import tvShows from "../models/tvshowModel.js"
 
+// const router = express.Router();
 
-// const router = express.Router()
-
-
-// router.get(`/`, async(req, res) => {
-//   try{
+// router.get('/', async (req, res) => {
+//   try {
 //     const query = req.query.query;
-//   if (!query) {
-//     return res.status(400).json({ error: 'Query parameter is required' });
-//   }else{
-//     const response = Movies.find({ "title": { $regex: /query/i } })
-//     console.log(response.data)
-
-//   }
-//   res.status(200).json(response.data)
-//   }catch(error){
-//     console.error(error)
+//     if (!query) {
+//       return res.status(400).json({ error: 'Query parameter is required' });
+//     } else {
+//       const response = await Movies.find({ "title": { $regex: new RegExp(query, 'i') } });
+//       const response2= await tvShows.find({"title": { $regex: new RegExp(query, 'i') }})
+//       console.log(response);
+//       res.status(200).json(response);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal server error' });
 //   }
 // });
 
-// export default router
+// export default router;
 import express from 'express';
 import Movies from "../models/movieModel.js";
+import tvShows from "../models/tvshowModel.js";
 
 const router = express.Router();
 
@@ -35,8 +35,12 @@ router.get('/', async (req, res) => {
     if (!query) {
       return res.status(400).json({ error: 'Query parameter is required' });
     } else {
-      const response = await Movies.find({ "title": { $regex: new RegExp(query, 'i') } });
-      console.log(response);
+      let response = await Movies.find({ "title": { $regex: new RegExp(query, 'i') } });
+      // If no results found in Movies, search in tvShows
+      if (response.length === 0) {
+        response = await tvShows.find({ "title": { $regex: new RegExp(query, 'i') } });
+      }
+      // console.log(response);
       res.status(200).json(response);
     }
   } catch (error) {
